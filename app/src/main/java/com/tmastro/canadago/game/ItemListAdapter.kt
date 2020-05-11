d39a4c568741abc2e3e62a295643df8fc29552ed
tmastrom <thomas.mastromonaco@gmail.com>
2020-05-11 16:36:25 -0700

    update list adapter

--- app/src/main/java/com/tmastro/canadago/game/ItemListAdapter.kt
+++ app/src/main/java/com/tmastro/canadago/game/ItemListAdapter.kt
@@ -7,15 +7,37 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tmastro.canadago.R
-import com.tmastro.canadago.TextItemViewHolder
 import com.tmastro.canadago.database.DataItem
 
 class ItemListAdapter: RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView){
         val name: TextView = itemView.findViewById(R.id.animal_name)
         val isFound: TextView = itemView.findViewById(R.id.is_found)
         val img: ImageView = itemView.findViewById(R.id.animal_image)
+
+        fun bind(item: DataItem) {
+            val res = itemView.context.resources
+            name.text = item.name
+            isFound.text = item.isFound.toString()
+            img.setImageResource(
+                when (item.name) {
+                    "Beaver" -> R.drawable.ic_pets_black_24dp
+                    "Goose" -> R.drawable.ic_pets_black_24dp
+                    "Moose" -> R.drawable.ic_pets_black_24dp
+                    else -> R.drawable.ic_launcher_foreground
+                }
+            )
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view =
+                    layoutInflater.inflate(R.layout.list_item_animal, parent, false)
+                return ViewHolder(view)
+            }
+        }
     }
 
     var data = listOf<DataItem>()
@@ -29,22 +51,11 @@ class ItemListAdapter: RecyclerView.Adapter<ItemListAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        //val res = holder.itemView.context.resources
-        holder.name.text = item.name
-        holder.isFound.text = item.isFound.toString()
-        holder.img.setImageResource(when (item.name) {
-            "Beaver" -> R.drawable.ic_pets_black_24dp
-            "Goose" -> R.drawable.ic_pets_black_24dp
-            "Moose" -> R.drawable.ic_pets_black_24dp
-            else -> R.drawable.ic_launcher_foreground
-        })
+        holder.bind(item)
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view =
-            layoutInflater.inflate(R.layout.list_item_animal, parent, false)
-        return ViewHolder(view)
+        return ViewHolder.from(parent)
     }
 }
